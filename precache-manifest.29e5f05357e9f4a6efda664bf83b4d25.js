dd1204b33d2706cfe9f10b63a4463cda95cdd75a
marie7900 <mmarion790@gmail.com>
2020-10-21 23:56:27 +0200

    Updates

---
+++ precache-manifest.29e5f05357e9f4a6efda664bf83b4d25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29866c515ef82a9d9c0156c756d14060",
+    "url": "/react-recipe-app/index.html"
+  },
+  {
+    "revision": "af5c1255eeafee1a5ca7",
+    "url": "/react-recipe-app/static/css/main.d030cac6.chunk.css"
+  },
+  {
+    "revision": "66eea809eb5dddeb6dc1",
+    "url": "/react-recipe-app/static/js/2.510e488c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-recipe-app/static/js/2.510e488c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af5c1255eeafee1a5ca7",
+    "url": "/react-recipe-app/static/js/main.2f862e2e.chunk.js"
+  },
+  {
+    "revision": "0e85480335cb53a85ec5",
+    "url": "/react-recipe-app/static/js/runtime-main.db934195.js"
+  },
+  {
+    "revision": "a727c0b8b6937b1b4d10ec3f0388068d",
+    "url": "/react-recipe-app/static/media/image.a727c0b8.png"
+  }
+]);
